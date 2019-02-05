8a7a1c494ab9b9e165855840425de24733ebf68d
Roman Bird <romanbird@gmx.com>
2019-02-05 18:04:56 +0000

    csv trimmer

---
+++ csv_trimmer.py
@@ -0,0 +1,8 @@
+import csv
+with open('db_sample.csv', "r+", newline='', encoding='utf8') as dbRaw:
+    initial=1
+    db = list(csv.reader(dbRaw, delimiter=","))
+    for i in db[1:]:
+        if int(i[-1])%5!=0:
+            i[-1]="delete_this"
+    writer=csv.writer(dbRaw)
