35ada67c914d897b99d919883be870621bb8bc6a
Roman Bird <romanbird@gmx.com>
2019-02-05 19:03:12 +0000

    trimmed csv file

--- csv_trimmer.py
+++ csv_trimmer.py
@@ -1,10 +1,10 @@
 import csv
 import pandas as pd
-dbRead = open('db_sample.csv', "r", newline='', encoding='utf8')
+dbRead = open('db.csv', "r", newline='', encoding='utf8')
 initial=1
 db = list(csv.reader(dbRead, delimiter=","))
 for i in db[1:]:
     if int(i[-1])%5!=0:
         i[-1]="delete_this"
 df=pd.DataFrame(data=db)
-df.to_csv('db_sample.csv', sep=",", encoding='utf8')
+df.to_csv('db.csv', sep=",", encoding='utf8')
