8c8e9972d974ecc8dcf167e08f60daa1a6c67004
Roman Bird <romanbird@gmx.com>
2019-02-05 18:08:48 +0000

    changed formatting of csv writer

--- csv_trimmer.py
+++ csv_trimmer.py
@@ -1,9 +1,9 @@
 import csv
-with open('db_sample.csv', "w+", newline='', encoding='utf8') as dbRaw:
-    initial=1
-    db = list(csv.reader(dbRaw, delimiter=","))
-    for i in db[1:]:
-        if int(i[-1])%5!=0:
-            i[-1]="delete_this"
-    writer=csv.writer(dbRaw)
-    writer.writerows(db)
+dbRaw = open('db_sample.csv', "w+", newline='', encoding='utf8')
+initial=1
+db = list(csv.reader(dbRaw, delimiter=","))
+for i in db[1:]:
+    if int(i[-1])%5!=0:
+        i[-1]="delete_this"
+writer=csv.writer(dbRaw, delimiter=",")
+writer.writerows(db)
