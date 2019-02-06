a3519bac809e1e68744a1d3171119796a4535b05
Roman Bird <romanbird@gmx.com>
2019-02-06 22:12:19 +0000

    counter prints current line

--- csv_counter.py
+++ csv_counter.py
@@ -2,7 +2,8 @@ import csv
 import pandas as pd
 dbRead = open('db.csv', "r", newline='', encoding='utf8')
 db = list(csv.reader(dbRead, delimiter=","))
-for row in db[1:]:
+for n,row in enumerate(db[1:]):
         row[-2]=sum(i.count(row[-1]) for i in db)
+        print("Line {} of {}".format(n+1,len(db)),end="\r",flush=True)
 df=pd.DataFrame(data=db)
 df.to_csv('db.csv', sep=",", encoding='utf8')
