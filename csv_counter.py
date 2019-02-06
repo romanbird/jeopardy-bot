f34cf5fb97b094c4da8e8c8e9bbf866f1def7a84
Roman Bird <romanbird@gmx.com>
2019-02-06 22:14:45 +0000

    progress bar using tqdm

--- csv_counter.py
+++ csv_counter.py
@@ -1,9 +1,9 @@
 import csv
 import pandas as pd
+from tqdm import tqdm
 dbRead = open('db.csv', "r", newline='', encoding='utf8')
 db = list(csv.reader(dbRead, delimiter=","))
-for n,row in enumerate(db[1:]):
+for row in tqdm(db[1:]):
         row[-2]=sum(i.count(row[-1]) for i in db)
-        print("Line {} of {}".format(n+1,len(db)),end="\r",flush=True)
 df=pd.DataFrame(data=db)
 df.to_csv('db.csv', sep=",", encoding='utf8')
