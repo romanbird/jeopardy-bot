584872da3cef68c0e60c48433d599bc66671f7aa
Roman Bird <romanbird@gmx.com>
2019-02-06 22:17:07 +0000

    only searches in column (massive optimisation)

--- csv_counter.py
+++ csv_counter.py
@@ -3,7 +3,8 @@ import pandas as pd
 from tqdm import tqdm
 dbRead = open('db.csv', "r", newline='', encoding='utf8')
 db = list(csv.reader(dbRead, delimiter=","))
+column = [row[-1] for row in db]
 for row in tqdm(db[1:]):
-        row[-2]=sum(i.count(row[-1]) for i in db)
+        row[-2]=sum(i.count(row[-1]) for i in column)
 df=pd.DataFrame(data=db)
 df.to_csv('db.csv', sep=",", encoding='utf8')
