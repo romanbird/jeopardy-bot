007838e0f8c55fd569fce7d0744e516e7465d9c9
Roman Bird <romanbird@gmx.com>
2019-02-07 20:31:06 +0000

    anticipate no more header columns

--- csv_trimmer.py
+++ csv_trimmer.py
@@ -2,7 +2,7 @@ import csv
 import pandas as pd
 dbRead = open('db.csv', "r", newline='', encoding='utf8')
 db = list(csv.reader(dbRead, delimiter=","))
-for i in db[1:]:
+for i in db:
     if int(i[-2])%5!=0:
         i[-2]="delete_this"
 df=pd.DataFrame(data=db)
