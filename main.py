db0184602ffa984d95cf30e8e1cb81e66185b475
Roman Bird <romanbird@gmx.com>
2019-02-09 09:40:50 +0000

    created returnUID func

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import csv
 class Question:
     def __init__(self, line):
-        print(line)
         self.id = line[0]
         if line[1]=="Jeopardy!":
             self.round=1
@@ -11,12 +10,17 @@ class Question:
         self.value=line[3]
         self.question=line[4]
         self.answer=line[5]
+    
+    def returnUID(self):
+        return self.id+self.topic
 
 def main():
     db = []
     with open('sample.csv', encoding='utf8') as dbraw:
         for i in list(csv.reader(dbraw)):
             db.append(i)
+    x=Question(db[0])
+    print(x.returnUID())
 
 if __name__ == "__main__":
     main()
